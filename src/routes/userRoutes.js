34a104e0776a1f884c1df6ae971c7211de44239c
Rahul Chaudhari <chaudhariraul5@gmail.com>
2025-12-08 06:03:20 +0545

    update routes with password recovery api

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,4 +6,8 @@ const router = express.Router();
 router.post("/signup", authController.signup);
 router.post("/login", authController.login);
 
+router.post("/forgotPassword", authController.forgotPassword); 
+router.post("/verifyOtp", authController.verifyPassOTP); 
+router.post("/resetPassword", authController.resetPassword); 
+
 module.exports = router;
